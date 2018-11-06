031c34354bae89ae60f346c94a34002de2c26a45
Piotr Tomczak <ptomek1997@gmail.com>
2018-11-06 20:40:43 +0100

    Unit Tests Added
    Just an outline though (does not work 100% yet)

---
+++ LibraryProoject/DataGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+namespace LibraryProojectTest
+{
+    public class DataGenerator
+    {
+        public DataGenerator()
+        {
+        }
+        public class TestDataGenerator : IEnumerable<object>
+        {
+            public static IEnumerable<object[]> GetBookFromDataGenerator()
+            {
+                yield return new object[]
+                {
+                     new Book { "title1", "author1", 1234, true, 4567 }
+                };
+            }
+
+        }
+    }
+}
