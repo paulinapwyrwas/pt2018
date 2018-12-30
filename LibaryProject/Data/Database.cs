6754dab3ec160e62336ca43619f7b509ef6a4419
unknown <paulina.p.wyrwas@gmail.com>
2018-12-30 08:52:26 -0800

    added LINQ SQL structure

---
+++ LibaryProject/Data/Database.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Data.Entity;
+
+namespace LibrarySolution
+{
+
+    public class Database : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Event> eventHistory { get; set; }
+        public DbSet<Reader> Readers { get; set; }
+
+    }
+}
